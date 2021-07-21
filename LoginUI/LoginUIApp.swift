01fa9be9963420bae5d455154c670d5545f6bfb1
Bhupender Rawat <bhupenderrawat742@gmail.com>
2021-07-21 11:22:16 +0530

    Initial Commit

---
+++ LoginUI/LoginUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginUIApp.swift
+//  LoginUI
+//
+//  Created by Bhupender Rawat on 21/07/21.
+//
+
+import SwiftUI
+
+@main
+struct LoginUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
